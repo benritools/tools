7fe080fb05ee9822a81454ee99c5f2d3b25cb8b0
benritools <ozaki.eri@gmail.com>
2026-03-06 11:22:06 +0900

    ver1.0 released

---
+++ apps/common/layout.js
@@ -0,0 +1,29 @@
+async function loadLayout(){
+
+  const headerEl = document.getElementById("header");
+  const footerEl = document.getElementById("footer");
+
+  if(headerEl){
+    const h = await fetch("../common/header.html");
+    headerEl.innerHTML = await h.text();
+  }
+
+  if(footerEl){
+    const f = await fetch("../common/footer.html");
+    footerEl.innerHTML = await f.text();
+  }
+}
+
+// スムーズスクロールの処理
+document.addEventListener('click', (e) => {
+  if (e.target && e.target.id === 'scrollTopBtn') {
+    e.preventDefault(); // 通常のリンク動作をキャンセル
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // 滑らかにスクロール
+    });
+  }
+});
+
+loadLayout();
+
